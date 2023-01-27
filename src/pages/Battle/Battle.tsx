bca4c4d5c4201bf3350b0ef54d68b0771d06204b
Aubis Sanchez <aubisst@gmail.com>
2023-01-27 01:28:56 -0400

    batte action section styles added

--- src/pages/Battle/Battle.tsx
+++ src/pages/Battle/Battle.tsx
@@ -1,6 +1,30 @@
+import { Stack } from "@mui/material"
+import { ActionsSection, BattleSectionContainer, PixelatedButton, SectionContainer } from "./style-components"
+import PlayerTeam from '../ChooseTeam/components/PlayerTeam';
+import Typography from "@mui/material/Typography";
+import { useSelector } from 'react-redux';
+import { AppStore } from '../../redux/store';
+
 const Battle = () => {
+  const { username } = useSelector( (store:AppStore) => store.user);
+
   return (
-    <div>Battle</div>
+    <SectionContainer>
+      <Stack width='75%' height='100%' direction="column">
+        <BattleSectionContainer>
+          
+          </BattleSectionContainer>
+          <ActionsSection>
+            <Stack direction="column" pl={5}>
+              <Typography  fontSize="32px" fontFamily="cursive" color="white" mt={-1}>What will </Typography>
+              <Typography  fontSize="32px" fontFamily="cursive" color="white" mt={-1}>{username.toUpperCase()} do? </Typography>
+            </Stack>
+            <PixelatedButton className="fightBtn" style={{ background:"#7b151e"}}>Fight</PixelatedButton>
+            <PixelatedButton className="changeBtn" style={{ background:"#bd6917"}}>Change</PixelatedButton>
+          </ActionsSection>
+      </Stack>
+      <PlayerTeam />
+    </SectionContainer>
   )
 }
 export default Battle
---
+++ src/pages/Battle/style-components/main.tsx
@@ -0,0 +1,46 @@
+import { styled } from '@mui/system';
+import battleBg from '../../../assets/battle_bg.webp';
+
+
+export const SectionContainer = styled("div")(({theme}) => ({
+    backgroundSize: 'cover',
+    height:"100vh",
+    width:"100vw",
+    display:"flex",
+    flexDirection:"row",
+    background: 'rgba(0,0,0,0.9)',
+}))
+
+export const BattleSectionContainer = styled("div")(({theme}) => ({ 
+    height:"80%",
+    width:"100%",
+    backgroundImage: `url(${battleBg})`,
+    
+}))
+
+export const ActionsSection = styled("div")(({theme}) => ({ 
+    height:"20%",
+    width:"100%",
+    border:"10px solid orange",
+    borderStyle:"double",
+    background:"linear-gradient( #21314b, #21314b,#21314b ) padding-box, blue",
+    display:"flex",
+    flexDirection:"row",
+    alignItems:"center",
+    justifyContent:"space-evenly"
+
+}))
+
+export const PixelatedButton = styled("button")(({theme}) => ({ 
+    fontSize:"40px",
+    height:"90%",
+    padding:"0 1.5em",
+    marginLeft:"0.5em",
+    fontFamily:"VT323",
+    borderRadius:"20px",
+    border:"2px solid white",
+    boxShadow:"0px 2px 5px rgba(0,0,0,1)",
+    color:"white",
+    cursor:"pointer",
+    letterSpacing:"3px"
+}))
