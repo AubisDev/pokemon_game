33fda9d20a57cfe6058fc807abaa8a8b69fa30f9
Aubis Sanchez <aubisst@gmail.com>
2023-02-04 01:54:41 -0400

    battle end message resposive

--- src/pages/Battle/components/BattleEnd.tsx
+++ src/pages/Battle/components/BattleEnd.tsx
@@ -26,12 +26,12 @@ const BattleEnd = ({message}:IBattleEnd) => {
         navigate(`/${PublicRoutes.HOME}`, {replace:true});
     }
   return (
-    <Box width='100%' height="100%" zIndex={500} position='absolute' display="flex" alignItems='center' flexDirection='column' justifyContent='center' color='white' sx={{ background:"rgba(0,0,0,.85)"}} >
+    <Box width='100%' height="100vh" zIndex={500} position='fixed' display="flex" alignItems='center' flexDirection='column' justifyContent='center' color='white' sx={{ background:"rgba(0,0,0,.85)"}} >
 
-        <Typography fontSize="48px"> Battle has ended</Typography>
-        <Typography fontSize="72px" fontWeight={600} color={color}> {message}</Typography>
+        <Typography fontSize={{xs:'28px',sm:"48px"}}> Battle has ended</Typography>
+        <Typography fontSize={{xs:'36px',sm:"72px"}} fontWeight={600} color={color}> {message}</Typography>
         <Box position='absolute' bottom='10%' display='flex' alignItems='center' flexDirection='column' justifyContent='center'> 
-            <Typography fontSize="30px" fontWeight={600} mb={1}> Thank you for playing</Typography>
+            <Typography fontSize={{xs:'22px',sm:"30px"}} fontWeight={600} mb={1}> Thank you for playing</Typography>
             <Button variant='contained' color='warning' onClick={handleResetGame}  sx={{ fontWeight:600, width:'250px', }}> Play again</Button>
         </Box>
     </Box>
